26d49587c20fea89ba8127be2e4dd730d181d625
DasKrisp <99538764+DasKrisp@users.noreply.github.com>
2022-10-03 11:15:43 +0200

    prevent errors if ClientPlayerEntity is not loaded

--- src/main/java/com/vandendaelen/nicephore/helper/PlayerHelper.java
+++ src/main/java/com/vandendaelen/nicephore/helper/PlayerHelper.java
@@ -5,10 +5,12 @@ import net.minecraft.text.MutableText;
 
 public class PlayerHelper {
     public static void sendHotbarMessage(MutableText translatableText) {
+        if (MinecraftClient.getInstance().player != null)
         MinecraftClient.getInstance().player.sendMessage(translatableText, true);
     }
 
     public static void sendMessage(MutableText translatableText) {
+        if (MinecraftClient.getInstance().player != null)
         MinecraftClient.getInstance().player.sendMessage(translatableText, false);
     }
 }
